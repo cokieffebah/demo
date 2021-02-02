c2987e97a583aa62d18ab0c10dcee97c4a8a073d
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-02 13:56:24 -0500

    added Jenkinsfile and pipeline_config.groovy

---
+++ Jenkinsfile
@@ -0,0 +1,4 @@
+
+build()
+scan()
+deploy()
---
+++ pipeline_config.groovy
@@ -0,0 +1,16 @@
+libraries{
+    in_toto_ex{
+        in_toto{
+            build{}
+            scan{}
+            deploy{}
+        }
+    }
+
+    in_toto_utils{
+
+    }
+
+}
+
+intotoCollector = []
