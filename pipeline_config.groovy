a92fa78cdcf3a9e70638cdc1ab7512073511316b
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-08 18:34:50 -0500

    using record and laoyt sub configs

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,23 +2,42 @@ libraries{
     in_toto_ex{
         in_toto{
             build{ 
-                materials = []
-                products = [["CREATE", "demo-project/*"], ["DISALLOW", "*"]]
-                key = "functionary_bob/bob"
+                record{
+                    materials = []
+                    products = "demo-project/*"
+                    key = "functionary_bob/bob"
+                }
+                
+                layout{
+                    materials = []
+                    products = [["CREATE", "demo-project/*"], ["DISALLOW", "*"]]
+                }
             }
             scan{
-                materials = [["MATCH", "demo-project/*", "WITH", "PRODUCTS", "FROM", "build"]]
-                products = [["CREATE", "scan.log"], ["DISALLOW", "*"]]
-                key = "functionary_bob/bob"
+                record{
+                    materials = "demo-project/*"
+                    products = "scan.log"
+                    key = "functionary_bob/bob"
+                }
+                layout{
+                    materials = [["MATCH", "demo-project/*", "WITH", "PRODUCTS", "FROM", "build"]]
+                    products = [["CREATE", "scan.log"], ["DISALLOW", "*"]]
+                }
             }
             
             package_app{
-                materials =  [
+                layout{
+                    materials =  [
                     ["MATCH", "demo-project/*", "WITH", "PRODUCTS", "FROM", "build"], 
                     ["MATCH", "scan.log", "WITH", "PRODUCTS", "FROM", "scan"], ["DISALLOW", "*"]]
 
-                products = [["CREATE", "demo-project.tar.gz"], ["DISALLOW", "*"]]
-                key = "functionary_carl/carl"
+                    products = [["CREATE", "demo-project.tar.gz"], ["DISALLOW", "*"]]
+                }
+                record{
+                    key = "functionary_carl/carl"
+                    materials =  "demo-project/* scan.log"
+                    products = "demo-project.tar.gz"
+                }
             }
             deploy{}
         }
